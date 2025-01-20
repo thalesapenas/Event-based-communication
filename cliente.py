d9f1f97512cf9bf1a92bb3301d1384dee27cdec5
Thales Rodrigues <139348568+thalesapenas@users.noreply.github.com>
2025-01-20 08:20:44 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -7,22 +7,22 @@ import json
 
 load_dotenv()
 
-# Defina o endereço do seu broker e as credenciais fornecidas
+
 broker = os.getenv("hive_server") 
 port = 8883  
-username = os.getenv("user") # Usuário fornecido pelo HiveMQ
-password = os.getenv("password")  # Senha fornecida pelo HiveMQ
+username = os.getenv("user") 
+password = os.getenv("password") 
 TOPIC = "meuprojeto/sensors/temperature"
 SENSOR_ID = "sensor_02"
 
-# Função chamada quando o cliente se conecta ao broker
+
 def on_connect(client, userdata, flags, rc):
     print(f"Conectado com o código de retorno: {rc}")
 
-# Função para publicar a temperatura
+
 def publish_temperature():
     while True:
-        # Simula um valor de temperatura
+        
         temperature = random.uniform(10.0, 40.0)
         payload = {
             "id": SENSOR_ID,
@@ -30,18 +30,18 @@ def publish_temperature():
         }
         client.publish(TOPIC, json.dumps(payload))
         print(f"Publicado: {payload}")
-        time.sleep(5)  # Publica a cada 5 segundos
+        time.sleep(5)  
 
-# Criando o cliente MQTT
+
 client = mqtt.Client()
 client.username_pw_set(username, password)  # Autenticação
-client.tls_set()  # Ativa TLS (para conexão segura)
+client.tls_set()  #TLS (para conexão segura)
 
-# Defina as funções de callback
+
 client.on_connect = on_connect
 
-# Conecte ao broker
+
 client.connect(broker, port, 60)
 
-# Inicie a publicação de temperatura
+
 publish_temperature()
